98dfa9be9358ecd53a07e6265855c61860151e0e
DearWang <475307228@qq.com>
2018-05-08 11:29:11 +0800

    测试修改

--- GitUploadTest/ViewController.m
+++ GitUploadTest/ViewController.m
@@ -18,20 +18,34 @@
     [super viewDidLoad];
     NSLog(@"这是一个测试Demo");
     
+    
+    UIButton *button =[UIButton buttonWithType:UIButtonTypeCustom];
+    
+    button.frame = CGRectMake(100, 100, 100, 100);
+    
+    button.backgroundColor =[UIColor redColor];
+    
+    [button addTarget:self action:@selector(buttonClick) forControlEvents:UIControlEventTouchUpInside];
+    
+    [self.view addSubview:button];
+    
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)buttonClick {
+    
+
     CABasicAnimation *animation = [CABasicAnimation animationWithKeyPath:@"position"];
     animation.duration = 2;
     animation.repeatCount = 1;
     animation.beginTime = CACurrentMediaTime() + 2;
     animation.autoreverses = YES;
     animation.timingFunction = [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseInEaseOut];
-//    animation.fromValue = [NSValue valueWithCGPoint:CGPointMake(0, 0)];
+    //    animation.fromValue = [NSValue valueWithCGPoint:CGPointMake(0, 0)];
     animation.toValue = [NSValue valueWithCGPoint:CGPointMake(320, 480)];
     [self.view.layer addAnimation:animation forKey:@"move-layer"];
     
-    // Do any additional setup after loading the view, typically from a nib.
 }
-
-
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
